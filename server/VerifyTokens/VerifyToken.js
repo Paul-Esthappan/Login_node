c112dcb9bef1e3bce323fd29e9e42e4254fecaf7
Paul-Esthappan <paulesthappan@gmail.com>
2023-10-05 16:15:16 +0530

    second

--- server/VerifyTokens/VerifyToken.js
+++ server/VerifyTokens/VerifyToken.js
@@ -1,21 +1,26 @@
 const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
-    console.log('header', req.header);
+    console.log('header', req.headers);
     
-    let authheader = req.header.verifyToken
+    let authheader = req.headers.token
     console.log('header token', authheader);
 
+
     if (authheader) {
-        const token = authheader.split(" ", [1]);
+        const token = authheader.split(" ")[1];
         jwt.verify(token,process.env.psw_KEY, (err, user) => {
-            if (err) res.status(403).json('token not valid')
-            console.log("MongoDB ID",user);
+            if (err) {
+                console.error("Token verification error:", err);
+                return res.status(403).json('Token not valid');
+            }
+
+            console.log("MongoDB ID", user);
             next();
-        })
+        });
+    } else {
+        // No authorization header
+        return res.status(403).json('You are not authenticated');
     }
-    else {
-        return res.status(403).jos('you are not authentacated')
-    }
-}
+};
 module.exports = {verifyToken}
